c615c7d21ed65000b8a7d26068a2b2d9871d7b25
Talita Mendonca Marques <skyzinha.talita1996@gmail.com>
2023-10-19 23:37:41 -0300

    Utiliznd a funç Map p - a verbosidade d nosso Códi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,25 @@
-const offset = 0;
-const limit = 10;
-const url: "https://pokeapi.co/api/v2/pokemon/1"
 
-fetch(url)
+function ConverterPokemon(pokemons) {
+    return `
+        li class="pokemon">
+            <span class="numero">#001</span>
+            <span class="nome">${pokemon.name}</span>
+
+            <div class="detalhes">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>                               
+        </li>
+
+`
+}
+
+const pokemonsList = document.getElementsById('pokemonsList')
+
+PokeApi.getPokemons().then((pokemons = []) => {
+    pokemonsList.innerHTML += pokemons.map(ConverterPokemon).join(``)    
+})
